21913d08f309eb405a4f14d628b734a803db9b50
Naiel Yohannes <naielyohannes@gmail.com>
2026-04-05 11:37:04 -0700

    Writing E2E tests for creating an account and updating the rest

--- Learning-tracker/tests/app.spec.js
+++ Learning-tracker/tests/app.spec.js
@@ -2,9 +2,37 @@
 import { test, expect } from '@playwright/test';
 import {  describe } from 'node:test';
 
+describe('testing user registration', () => {
+  test('user can register with correct credentials', async({page}) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', {name: 'Create new account'}).click()
+    await page.getByLabel('Username: ').fill(`new tester${Date.now()}`)
+    await page.getByLabel('Name: ', {exact: true}).fill('tester')
+    await page.getByLabel('Password: ').fill('Testers_code123!')
+    await page.getByRole('button', {name: 'Register'}).click()
+
+    await expect(page.getByText('My Learning Tracker')).toBeVisible({ timeout: 10000 })
+  })
+
+  test('user cant register with missing credentials', async({page}) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', {name: 'Create new account'}).click()
+    await page.getByLabel('Username: ').fill(`wrong tester${Date.now()}`)
+    await page.getByLabel('Name: ', {exact: true}).fill('failed')
+    await page.getByLabel('Password: ').fill('')
+    await page.getByRole('button', {name: 'Register'}).click()
+
+    await expect(page.getByText('My Learning Tracker')).not.toBeVisible()
+  })
+})
+
+
 describe('testing logging in and logging out', () => {
   test('user can login successfully', async ({ page }) => {
     await page.goto('http://localhost:5173')
+    await page.getByRole('button', {name: 'Login'}).click()
 
     await page.getByLabel('username').fill('test')
     await page.getByLabel('password').fill('Secret.password?123')
@@ -17,21 +45,22 @@ describe('testing logging in and logging out', () => {
 
   test('user can successfully loggout', async({page}) => {
     await page.goto('http://localhost:5173')
-
+    await page.getByRole('button', {name: 'Login'}).click()
+    
     await page.getByLabel('username').fill('test')
     await page.getByLabel('password').fill('Secret.password?123')
     await page.getByRole('button', {name: 'login'}).click()
 
     await page.getByRole('button', {name: 'loggout'}).click()
 
-    const locator = page.getByText('Login Form')
-    await expect(locator).toBeVisible()
+    await expect(page.getByText(/Welcome to/)).toBeVisible()
   })
 })
 
 describe('testing the topics managment', () => {
   test.beforeEach(async({page}) => {
     await page.goto('http://localhost:5173')
+    await page.getByRole('button', {name: 'Login'}).click()
     await page.getByLabel('username').fill('test')
     await page.getByLabel('password').fill('Secret.password?123')
     await page.getByRole('button', {name: 'login'}).click()
@@ -43,7 +72,7 @@ describe('testing the topics managment', () => {
     await textBoxes[1].fill('5')
     await page.getByRole('button', {name: 'add'}).click()
 
-    await expect(page.getByText('New test topic').first()).toBeVisible()
+    await expect(page.getByText('New test topic').first()).toBeVisible({ timeout: 10000 })
   })
 
   test('updating a topic will work', async ({ page }) => {
